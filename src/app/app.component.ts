c284d70b874c358899c166d4daba32215cba6f96
Vikram <vikram@gmail.com>
2018-04-23 22:59:12 -0400

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -23,21 +23,22 @@ export class AppComponent implements OnInit {
 
     let reqObj: Object = {
         method : "GET",
-        url    : "assets/sampledata.json"
+        // url    : "assets/sampledata.json"
+        url : 'http://api.openweathermap.org/data/2.5/box/city?bbox=12,32,15,37,10&appid=ea59b2f6c7845b7c614afc19724cf4ad'
     }
     // let reqObj = this._utilService.createHttpReqObj("GET", "../assets/json/getFacilityInfoByAssetId.json", reqParam);
     this.weatherService.getWeatherData(reqObj)
       .subscribe(
         response => {
-          let result = response[1].list;
+          let result = response.list;
           this.weatherData = result.sort((a, b) => a.name.localeCompare(b.name));
           console.log("result :" + JSON.stringify(response)); //data.headers._headers.get("authorization")
           // if(response.header.statusCode == 0){
-            
+
           // }
           // else{
           //   console.log("no response");
-          // }                
+          // }
 
       }, //success
       error => {
@@ -45,11 +46,11 @@ export class AppComponent implements OnInit {
       },
       () => {
         console.log('call finished');
-        
+
       }
 
-    );   
-    
+    );
+
   }
 
 
